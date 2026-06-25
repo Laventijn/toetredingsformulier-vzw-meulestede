14a8b77e9431b9d6394e888902469311fb61b6fb
Valentijn Rombaut <valentijn@rmbt.be>
2026-06-26 00:55:27 +0200

    Configure Google Apps Script backend

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,6 +3,6 @@
 window.APP_CONFIG = {
   // Web app URL uit Google Apps Script, bijvoorbeeld:
   // https://script.google.com/macros/s/AKfycbx.../exec
-  GOOGLE_SCRIPT_URL: "",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwl8GkYuKVwlWaNwYDgpNZOZf_K5XaxpVB9XPy1vDMHVFH8OAZm_0OrbgStTpPRpfP0/exec",
   ORGANISATION_EMAIL: "info@meulestede.gent"
 };
